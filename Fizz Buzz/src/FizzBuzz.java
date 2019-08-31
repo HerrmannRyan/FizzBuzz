fe2e3cdd29cd5cb566fd60b025c4059d1ac747da
rybla <rybla@Ryans-PC>
2019-08-31 11:35:20 -0600

    Working Fizz Buzz in one module

--- Fizz Buzz/src/FizzBuzz.java
+++ Fizz Buzz/src/FizzBuzz.java
@@ -2,9 +2,29 @@
 public class FizzBuzz
 	{
 
+		// This is  a working now for FizzBuzz Game. Does it need to be in separate modules?
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
+				
+				for (int i = 1; i <= 100; i++)
+				{
+					if ((i % 5 == 0) && (i % 3 ==0))
+					{
+						System.out.println("FizzBuzz");
+					}
+					else if (i % 3 == 0)
+					{
+						System.out.println("Fizz");
+					}
+					else if (i % 5 == 0)
+					{
+						System.out.println("Buzz");
+					}
+					else
+					{
+						System.out.println(i);
+					}
+				}
 
 			}
 
